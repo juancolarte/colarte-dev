73f70ebae85770947287ba439e575d625ca5247e
colarte <colarte@colarte.no-ip.org.no-ip.org>
2009-12-05 04:45:59 -0500

    working console at nuevo.py

--- remote_control.py
+++ remote_control.py
@@ -10,6 +10,7 @@ appengine_dirs = ['/home/colarte/dev/google_appengine/']
 sys.path.extend(appengine_dirs)
 my_root_dir = os.path.abspath(os.path.dirname('/home/colarte/dev/python/GAE/colarte-dev/'))
 sys.path.insert(0, my_root_dir)
+
 APP_NAME = 'colarte-dev'
 os.environ['AUTH_DOMAIN'] = 'gmail.com'
 
@@ -20,3 +21,7 @@ def auth_func():
 # servername='localhost:8080'
 # Otherwise, remote_api assumes you are targeting APP_NAME.appspot.com
 remote_api_stub.ConfigureRemoteDatastore(APP_NAME, '/remote_api', auth_func)
+from myapp.models.urlstore import Category
+categories = Category.all()
+for cat in categories:
+    print cat.category
